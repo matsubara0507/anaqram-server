e109dc13f772329c4608c1cc246b06810638187b
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2017-10-19 07:29:57 +0000

    Change mysql

--- src/DB.hs
+++ src/DB.hs
@@ -20,9 +20,8 @@ import           Control.Monad.Trans.Reader   (runReaderT)
 import           Control.Monad.Trans.Resource (ResourceT, runResourceT)
 import           Data.ByteString              ()
 import           Data.Maybe                   (fromMaybe)
-import           Data.String                  (IsString(fromString))
 import           Database.Persist
-import           Database.Persist.Postgresql
+import           Database.Persist.MySQL
 import           Database.Persist.TH
 import           Score                        (Score (Score))
 import           System.Environment           (lookupEnv)
@@ -41,32 +40,42 @@ toScore (ScoreData a b c) = Score a b c
 fromScore :: Score -> ScoreData
 fromScore (Score a b c) = ScoreData a b c
 
-getConn :: String -> ConnectionString
-getConn pass =  mconcat 
-    [ "host=127.0.0.1", " "
-    , "port=5431", " "
-    , "sslmode=disable", " "
-    , "dbname=postgres", " "
-    , "user=postgres", " "
-    , "password=", fromString pass
-    ]
+getConn :: IO ConnectInfo
+getConn = do
+  let 
+    dbName = "MYSQL_"
+    info = [ ("HOST", "127.0.0.1")
+           , ("PORT", "3306")
+           , ("USER", "root")
+           , ("PASSWORD", "")
+           , ("DATABASE", "anaqram")
+           ]
+  [host, port, user, pass, db] <-
+    mapM (\(k, v) -> fromMaybe v <$> lookupEnv (dbName ++ k)) info
+  return $ defaultConnectInfo 
+    { connectHost = host
+    , connectPort = read port
+    , connectUser = user
+    , connectPassword = pass
+    , connectDatabase = db 
+    }
 
-runDB :: ConnectionString -> SqlPersistT (ResourceT (NoLoggingT IO)) a -> IO a
-runDB conn = runNoLoggingT . runResourceT . withPostgresqlConn conn . runSqlConn
+runDB :: ConnectInfo -> SqlPersistT (ResourceT (NoLoggingT IO)) a -> IO a
+runDB conn = runNoLoggingT . runResourceT . withMySQLConn conn . runSqlConn
 
 doMigration :: IO ()
 doMigration = do
-  conn <- getConn . fromMaybe "postgres" <$> lookupEnv "POSTGRESQL_PASSWORD"
-  runNoLoggingT . runResourceT . withPostgresqlConn conn . runReaderT $ runMigration migrateAll
+  conn <- getConn 
+  runNoLoggingT . runResourceT . withMySQLConn conn . runReaderT $ runMigration migrateAll
 
 selectScores :: IO [Score]
 selectScores = do
-    conn <- getConn . fromMaybe "postgres" <$> lookupEnv "POSTGRESQL_PASSWORD"
+    conn <- getConn
     scoreList <- runDB conn  $ selectList [] []
     return $ map (\(Entity _ u) -> toScore u) scoreList
 
 insertScore :: Score -> IO ()
 insertScore score = do
-    conn <- getConn . fromMaybe "postgres" <$> lookupEnv "POSTGRESQL_PASSWORD"
+    conn <- getConn
     runDB conn . insert_ . fromScore $ score
 
