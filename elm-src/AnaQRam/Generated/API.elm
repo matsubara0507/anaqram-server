4e360831c7da009c253d9f757baf59efdf76e963
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2019-10-05 12:50:06 +0900

    Refactor: elm format

--- elm-src/AnaQRam/Generated/API.elm
+++ elm-src/AnaQRam/Generated/API.elm
@@ -1,79 +1,90 @@
-module AnaQRam.Generated.API exposing(..)
+module AnaQRam.Generated.API exposing (getApiProblem, getApiSizes, maybeBoolToIntStr)
 
+-- The following module comes from bartavelle/json-helpers
+
+import Dict exposing (Dict)
+import Http
 import Json.Decode
 import Json.Encode exposing (Value)
--- The following module comes from bartavelle/json-helpers
 import Json.Helpers exposing (..)
-import Dict exposing (Dict)
 import Set
-import Http
 import String
 import Url.Builder
 
+
 maybeBoolToIntStr : Maybe Bool -> String
 maybeBoolToIntStr mx =
-  case mx of
-    Nothing -> ""
-    Just True -> "1"
-    Just False -> "0"
+    case mx of
+        Nothing ->
+            ""
+
+        Just True ->
+            "1"
+
+        Just False ->
+            "0"
 
 
-getApiSizes : (Result Http.Error  ((List Int))  -> msg) -> Cmd msg
+getApiSizes : (Result Http.Error (List Int) -> msg) -> Cmd msg
 getApiSizes toMsg =
     let
         params =
             List.filterMap identity
-            (List.concat
-                [])
+                (List.concat
+                    []
+                )
     in
-        Http.request
-            { method =
-                "GET"
-            , headers =
-                []
-            , url =
-                Url.Builder.crossOrigin ""
-                    [ "api"
-                    , "sizes"
-                    ]
-                    params
-            , body =
-                Http.emptyBody
-            , expect =
-                Http.expectJson toMsg (Json.Decode.list (Json.Decode.int))
-            , timeout =
-                Nothing
-            , tracker =
-                Nothing
-            }
+    Http.request
+        { method =
+            "GET"
+        , headers =
+            []
+        , url =
+            Url.Builder.crossOrigin ""
+                [ "api"
+                , "sizes"
+                ]
+                params
+        , body =
+            Http.emptyBody
+        , expect =
+            Http.expectJson toMsg (Json.Decode.list Json.Decode.int)
+        , timeout =
+            Nothing
+        , tracker =
+            Nothing
+        }
 
-getApiProblem : Int -> (Result Http.Error  (String)  -> msg) -> Cmd msg
+
+getApiProblem : Int -> (Result Http.Error String -> msg) -> Cmd msg
 getApiProblem query_size toMsg =
     let
         params =
             List.filterMap identity
-            (List.concat
-                [ [ Just query_size
-                    |> Maybe.map (String.fromInt >> Url.Builder.string "size") ]
-                ])
-    in
-        Http.request
-            { method =
-                "GET"
-            , headers =
-                []
-            , url =
-                Url.Builder.crossOrigin ""
-                    [ "api"
-                    , "problem"
+                (List.concat
+                    [ [ Just query_size
+                            |> Maybe.map (String.fromInt >> Url.Builder.string "size")
+                      ]
                     ]
-                    params
-            , body =
-                Http.emptyBody
-            , expect =
-                Http.expectJson toMsg Json.Decode.string
-            , timeout =
-                Nothing
-            , tracker =
-                Nothing
-            }
+                )
+    in
+    Http.request
+        { method =
+            "GET"
+        , headers =
+            []
+        , url =
+            Url.Builder.crossOrigin ""
+                [ "api"
+                , "problem"
+                ]
+                params
+        , body =
+            Http.emptyBody
+        , expect =
+            Http.expectJson toMsg Json.Decode.string
+        , timeout =
+            Nothing
+        , tracker =
+            Nothing
+        }
